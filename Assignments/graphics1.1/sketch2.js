8f6389d37097ae70000c1d1bb37e669ce588c812
Nathan Pham-Le <96894478+mtpham017@users.noreply.github.com>
2025-01-21 13:09:53 -0600

    format cahnges

---
+++ Assignments/graphics1.1/sketch2.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    colorMode(HSB);
+  
+  }
+  
+  function draw() {
+    background(130,90,100);
+  
+    square(1000,150,500);
+  
+    circle(500,400,500);
+  }
+  

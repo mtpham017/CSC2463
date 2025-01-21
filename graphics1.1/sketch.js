280bd39a7087ecb97a301487d9f98e5952306d91
Nathan Pham-Le <96894478+mtpham017@users.noreply.github.com>
2025-01-21 12:35:42 -0600

    1.1

---
+++ graphics1.1/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB);
+
+}
+
+function draw() {
+  background(130,90,100);
+
+  square(1000,150,500);
+
+  circle(500,400,500);
+}

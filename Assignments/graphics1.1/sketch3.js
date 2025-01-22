8eadef2bfac274d5a266cced8fc6cabf55e864ac
Nathan Pham-Le <96894478+mtpham017@users.noreply.github.com>
2025-01-22 10:55:00 -0600

    pacman

--- Assignments/graphics1.1/sketch3.js
+++ Assignments/graphics1.1/sketch3.js
@@ -6,22 +6,52 @@ function setup() {
   function draw() {
     background(0, 0, 0);
   
-    
+    //yellow
     let c1 = color(60, 100, 100);
+
+     //red
+     let c2 = color(10,100,100);
+
+     //blue
+     let c3 = color(220,100,50);
+
+     //white
+     let c4 = color(0,0,100)
   
-    
+    //pacman
     fill(c1);
     noStroke();
-    circle(550, 400, 600);
-  
-    
-    fill(0, 0, 0); 
+    arc(550, 400, 600, 600, radians(220), radians(500));
+
+    //ghost
+    fill(c2);
     noStroke();
-    triangle(
-      550, 400,                 
-      550 - 300, 400 - 150,     
-      550 - 300, 400 + 150      
-    );
+    arc(1300, 400, 600, 600, PI, 0, CHORD);
+    rect(1000, 400, 600, 300);
+
+    //eyes
+    fill(c4);
+    noStroke();
+    circle(1170,350,170);
+    circle(1450,350,170);
+
+    fill(c3);
+    noStroke;
+    circle(1170,350,100);
+    circle(1450,350,100);
+   
+
+    
+
+
+
+
+
+
+
+
+
+
   }
   
   

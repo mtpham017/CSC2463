1790f9df64fc60c913917f0735829a41701ebe47
Nathan Pham-Le <96894478+mtpham017@users.noreply.github.com>
2025-01-23 09:41:20 -0600

    star

--- Assignments/graphics1.1/sketch4.js
+++ Assignments/graphics1.1/sketch4.js
@@ -26,6 +26,27 @@ function setup() {
     fill(c2)
     noStroke();
     circle(windowWidth/2, windowHeight/2, 580);
-  
+
+    fill(c1);
+    noStroke();
+    star(windowWidth / 2, windowHeight / 2, 120, 310, 5, -PI / 2); 
+
+    fill(c3);
+    noStroke();
+    star(windowWidth / 2, windowHeight / 2, 110, 290, 5, -PI / 2);
   }
   
+  function star(x, y, radius1, radius2, npoints, rotation = 0) {
+    let angle = TWO_PI / npoints;
+    let halfAngle = angle / 2.0;
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += angle) {
+      let sx = x + cos(a + rotation) * radius2;
+      let sy = y + sin(a + rotation) * radius2;
+      vertex(sx, sy);
+      sx = x + cos(a + halfAngle + rotation) * radius1;
+      sy = y + sin(a + halfAngle + rotation) * radius1;
+      vertex(sx, sy);
+    }
+    endShape(CLOSE);
+  }
